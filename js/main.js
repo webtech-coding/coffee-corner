23d616d49b735642accd676d550051948628b88b
shovit <shovitthapa@gmail.com>
2018-04-09 06:43:16 +0300

    SERVE AND ANIMATION

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 window.sr = ScrollReveal();
 sr.reveal('.description');
 sr.reveal('.special');
+sr.reveal('.found__header');
+sr.reveal('.found__header-year');
 
 $('.special__food').slick({
     infinite: true,
